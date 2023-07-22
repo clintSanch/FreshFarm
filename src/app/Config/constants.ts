2af9630e02b7ff7bd1d3d5082672b27f377b03b4
clintSanch <clintonodira@gmail.com>
2023-07-22 19:17:46 -0400

    delta in default env in angular.json

--- src/app/Config/constants.ts
+++ src/app/Config/constants.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable()
 
 export class Constants {
+    
     public readonly api_Endpoints: string = 'http://localhost:5000/';
 
     public readonly mock_Endpoints: string = 'https://localhost:5000/';
